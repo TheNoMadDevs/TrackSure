eed46adca04f1d3eabc98b9f2e6750e8050ba6a8
hsanirudh <hsanirudh@gmail.com>
2024-12-13 02:56:00 +0530

    frontend: fix ThemeProvider
    Signed-off-by: hsanirudh <hsanirudh@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,25 @@ import routes from '@routes/RoutesConfig';
 import HomePage from '@pages/Home/HomePage';
 import SignUp from '@pages/Home/SignUp';
 import SignIn from '@pages/Home/SignIn';
+import { ThemeProvider } from '@components/common/ThemeProvider';
+
 
 const App = () => {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      {routes.map((route) => (
-        <Route
-          key={route.path}
-          path={route.path}
-          element={<ProtectedRoute element={route.element} roles={route.roles} />}
-        />
-      ))}
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<ProtectedRoute element={route.element} roles={route.roles} />}
+          />
+        ))}
+      </Routes>
+    </ThemeProvider>
   );
 };
 
